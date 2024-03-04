1ceb5c146f6456254f6c2b074a1f5ceb4763cda6
SaarthakMaini <saarthakmaini@gmail.com>
2024-03-04 11:30:27 +0530

    Prevention

---
+++ dosPrevention.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8.20;
+
+contract KingOfEther {
+    address public king;
+    uint256 public balance;
+    mapping(address => uint256) public balances;
+
+    function claimThrone() external payable {
+        require(msg.value > balance, "Need to pay more to become the king");
+
+        balances[king] += balance;
+
+        balance = msg.value;
+        king = msg.sender;
+    }
+
+    function withdraw() public {
+        require(msg.sender != king, "Current king cannot withdraw");
+
+        uint256 amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+
+        (bool sent,) = msg.sender.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+}
+
+contract Attack {
+    KingOfEther kingOfEther;
+    constructor(KingOfEther _kingOfEther) {
+        kingOfEther = KingOfEther(_kingOfEther);
+    }
+    function attack() public payable {
+        kingOfEther.claimThrone{value: msg.value}();
+    }
+}
